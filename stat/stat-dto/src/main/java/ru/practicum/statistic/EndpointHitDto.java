a6f48a4970b7dcd2a518a66b675673a51a27ceed
Elena16-beep <elenanikolaevaya@yandex.ru>
2025-11-22 00:58:16 +0500

    Проект ExploreWithMe. Этап 1. Сервис статистики. Dto для сервиса статистики (спринт 17) (#10)
    * Проект ExploreWithMe. Этап 1. Сервис статистики. Dto для сервиса статистики (спринт 17)
    
    * Проект ExploreWithMe. Этап 1. Сервис статистики. Dto для сервиса статистики. v.2 Изменила docker-compose (спринт 17)
    
    * Проект ExploreWithMe. Этап 1. Сервис статистики. Dto для сервиса статистики. v.2 Добавлен main-service (спринт 17)
    
    * Проект ExploreWithMe. Этап 1. Сервис статистики. Dto для сервиса статистики. v.4 Добавлен main-service (спринт 17)
    
    * Проект ExploreWithMe. Этап 1. Сервис статистики. Dto для сервиса статистики. v.5 Добавлен Dockerfile (спринт 17)
    
    * Dto для сервиса статистики. v.6 Добавлен Dockerfile
    
    * Dto для сервиса статистики. v.7 docker-compose
    
    * Dto для сервиса статистики. v.8 docker-compose
    
    * Dto для сервиса статистики. v.8 docker-compose
    
    * Dto для сервиса статистики. v.9 docker-compose
    
    * Dto для сервиса статистики. v.10 pom для main-service
    
    * Dto для сервиса статистики. v.11 docker-compose
    
    * change module names
    
    ---------
    
    Co-authored-by: fzk <fzk@fzk.fzk>

---
+++ stat/stat-dto/src/main/java/ru/practicum/statistic/EndpointHitDto.java
@@ -0,0 +1,22 @@
+package ru.practicum.statistic;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.Data;
+
+@Data
+public class EndpointHitDto {
+    private Long id;
+
+    @NotBlank
+    private String app;
+
+    @NotBlank
+    private String uri;
+
+    @NotBlank
+    private String ip;
+
+    @NotNull
+    private String timestamp;
+}
