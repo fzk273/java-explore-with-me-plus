63542d0064f10463c79ca936de97e23ce140d0dc
lipsiluya <lipsilu@yandex.ru>
2025-11-23 12:15:01 +0300

    реализовал статистику сервера (#13)
    * реализовал всю статистику сервера
    
    * исправил .gitignore
    
    * Remove H2 database file
    
    * создал dockerfile и поправил корневой docker-compose
    
    * f
    
    * f
    
    * refactor folders
    add annotation to stat-client to fix compilation
    
    * add actuator
    
    * f
    
    * локально прошли все 12 тестов, поправил repository
    
    * поправил родительским pom
    
    * fix properties file
    add local properties file
    fix code style
    disable unused services in docker-compose.yml
    
    * исправил SQL запросы в репозитории, пофиксил DTO и добавил обработку опциональных параметров в сервисе
    
    * f
    
    ---------
    
    Co-authored-by: Maxim Telegin <maximtelegin@MacBook-Pro-Maxim.local>
    Co-authored-by: fzk <fzk@fzk.fzk>

--- stat/stat-client/src/main/java/ru/practicum/statistic/StatisticClient.java
+++ stat/stat-client/src/main/java/ru/practicum/statistic/StatisticClient.java
@@ -1,4 +1,12 @@
 package ru.practicum.statistic;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class StatisticClient {
+    public static void main(String[] args) {
+        SpringApplication.run(StatisticClient.class, args);
+    }
+
 }
