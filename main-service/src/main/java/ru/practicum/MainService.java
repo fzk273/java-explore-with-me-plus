b48eb6f54c9de45b36b722dbd3675194a95bc688
fzk <fzk@fzk.fzk>
2025-12-06 20:28:24 +0100

    Need Help!!!

--- main-service/src/main/java/ru/practicum/MainService.java
+++ main-service/src/main/java/ru/practicum/MainService.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class MainService {
+    //TODO мы уже сломали голову и не можем победить последние 4 теста. Нужна помощь.
     public static void main(String[] args) {
         SpringApplication.run(MainService.class, args);
     }
