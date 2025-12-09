dc9e153212edb1cb33b3cce9d111318dcc28351a
fzk <fzk@fzk.fzk>
2025-12-09 19:49:25 +0100

    add private comments controller feature

---
+++ main-service/src/main/java/ru/practicum/comment/dto/NewCommentDto.java
@@ -0,0 +1,19 @@
+package ru.practicum.comment.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class NewCommentDto {
+
+    @NotBlank
+    @Size(min = 1, max = 3000)
+    private String text;
+}
---
+++ main-service/src/main/java/ru/practicum/comment/service/CommentServiceInterface.java
@@ -0,0 +1,18 @@
+package ru.practicum.comment.service;
+
+import jakarta.validation.Valid;
+import ru.practicum.comment.dto.CommentFullDto;
+import ru.practicum.comment.dto.NewCommentDto;
+import ru.practicum.comment.dto.UpdCommentDto;
+
+import java.util.List;
+
+public interface CommentServiceInterface {
+    CommentFullDto addComment(@Valid NewCommentDto dto, Long eventId, Long userId);
+
+    CommentFullDto updateComment(Long userId, Long eventId, Long commentId, @Valid UpdCommentDto updDto);
+
+    void deleteComment(Long userId, Long eventId, Long commentId);
+
+    List<CommentFullDto> getAllCommentsBy(Long userId, Long eventId);
+}
