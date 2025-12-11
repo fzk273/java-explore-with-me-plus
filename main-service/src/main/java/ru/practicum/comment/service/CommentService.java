d59edd6f6b93edc20dd8366644dede3729e672ac
fzk <fzk@fzk.fzk>
2025-12-11 15:36:47 +0100

    add PublicCommentController add PrivateCommentController

--- main-service/src/main/java/ru/practicum/comment/service/CommentService.java
+++ main-service/src/main/java/ru/practicum/comment/service/CommentService.java
@@ -9,10 +9,13 @@ import ru.practicum.comment.dto.NewCommentDto;
 import ru.practicum.comment.dto.UpdCommentDto;
 import ru.practicum.comment.mapper.CommentMapper;
 import ru.practicum.comment.model.Comment;
+import ru.practicum.comment.model.CommentState;
 import ru.practicum.comment.repository.CommentRepository;
 import ru.practicum.event.dao.EventRepository;
 import ru.practicum.event.model.Event;
+import ru.practicum.event.model.EventState;
 import ru.practicum.exception.NotFoundException;
+import ru.practicum.exception.NotPublishEventException;
 import ru.practicum.user.model.User;
 import ru.practicum.user.repository.UserRepository;
 
@@ -29,6 +32,7 @@ public class CommentService implements CommentServiceInterface {
     private final EventRepository eventRepository;
     private final CommentRepository commentRepository;
 
+
     @Override
     public CommentFullDto addComment(@Valid NewCommentDto dto, Long eventId, Long userId) {
         Optional<Event> event = eventRepository.findByIdAndInitiatorId(eventId, userId);
@@ -44,6 +48,53 @@ public class CommentService implements CommentServiceInterface {
         return CommentMapper.toFullDto(createdComment);
 
     }
+
+    @Override
+    public CommentFullDto updateCommentStatusByAdmin(Long eventId, Long commentId, boolean published) {
+        Optional<Comment> commentOpt = commentRepository.findByIdAndEventId(commentId, eventId);
+
+        if (commentOpt.isEmpty()) {
+            log.error("Comment with id {} not found for event {}", commentId, eventId);
+            throw new NotFoundException("Comment not found for the specified event");
+        }
+
+        Comment comment = commentOpt.get();
+        comment.setState(published ? CommentState.PUBLIC : CommentState.HIDDEN);
+
+        Comment updatedComment = commentRepository.save(comment);
+
+        log.info("Comment with id {} status changed to {}", commentId,
+                published ? "PUBLIC" : "HIDDEN");
+
+        return CommentMapper.toFullDto(updatedComment);
+    }
+
+    @Override
+    public List<CommentFullDto> getPublicCommentsByEvent(Long eventId) {
+        Optional<Event> eventOpt = eventRepository.findById(eventId);
+
+        if (eventOpt.isEmpty()) {
+            log.error("Event with id {} not found", eventId);
+            throw new NotFoundException("Event with id " + eventId + " not found");
+        }
+
+        Event event = eventOpt.get();
+
+        if (event.getState() != EventState.PUBLISHED) {
+            log.error("Event with id {} is not published", eventId);
+            throw new NotPublishEventException("Event with id " + eventId + " is not published");
+        }
+
+        List<Comment> comments = commentRepository.findPublicCommentsByEventId(eventId, CommentState.PUBLIC);
+        if (comments.isEmpty()) {
+            return Collections.emptyList();
+        }
+
+        return comments.stream()
+                .map(CommentMapper::toFullDto)
+                .toList();
+    }
+
 
     @Override
     public CommentFullDto updateComment(Long userId, Long eventId, Long commentId, @Valid UpdCommentDto updDto) {
@@ -63,6 +114,7 @@ public class CommentService implements CommentServiceInterface {
         return CommentMapper.toFullDto(commentToUpdate);
     }
 
+
     @Override
     public void deleteComment(Long userId, Long eventId, Long commentId) {
         Optional<Event> event = eventRepository.findByIdAndInitiatorId(eventId, userId);
@@ -78,6 +130,7 @@ public class CommentService implements CommentServiceInterface {
         commentRepository.delete(comment.get());
     }
 
+
     @Override
     public List<CommentFullDto> getAllCommentsBy(Long userId, Long eventId) {
         Optional<User> user = userRepository.findById(userId);
@@ -90,7 +143,7 @@ public class CommentService implements CommentServiceInterface {
             log.error("there is no such event: " + eventId);
             throw new NotFoundException("there is no such event: " + eventId);
         }
-        List<Comment> commentList = commentRepository.findByUserIdAndEventId(user.get().getId(), event.get().getId());
+        List<Comment> commentList = commentRepository.findByAuthorIdAndEventId(user.get().getId(), event.get().getId());
         if (commentList.isEmpty()) {
             return Collections.emptyList();
         }
